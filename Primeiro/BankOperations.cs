8892f1f0d0fa35ab6b81792b46957766b028a1b4
Tonineto <netotoni89-glitch>
2026-07-07 09:13:07 -0300

    Sistema Bancario

---
+++ Primeiro/BankOperations.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Primeiro
+{
+    public class BankOperations
+    {
+        decimal balance = 1000;
+        public void CheckBalance()
+        {
+            Console.WriteLine($"Seu saldo e de R${balance}");
+        }
+
+        public void Deposit()
+        {
+            Console.WriteLine("informe o valor a ser depositado: R$");
+
+            if (decimal.TryParse(Console.ReadLine(), out decimal amount) && amount > 0)
+
+            {
+                balance += amount;
+            }
+            else
+            {
+                Console.WriteLine("Valor invalido. Tente novamente.");
+            }
+        }
+
+        public void Witdraw()
+        {
+            Console.WriteLine("informe o valor a ser retirado: R$");
+
+            if (decimal.TryParse(Console.ReadLine(), out decimal amount) && amount > 0)
+            {
+                if (amount <= balance)
+                {
+                    balance = balance - amount;
+                    Console.WriteLine($"Valor de  R${amount} retirado com sucesso!");
+                }
+                else
+                {
+                    Console.WriteLine("Saldo Invalido.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor invalido. tente novamente.");
+            }
+
+
+        }
+    }
+}
+
+
+       
